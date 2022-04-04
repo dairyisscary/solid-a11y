6f5eb5eda452997354844488cf01a6e434d2f4f2
Eric Kim-Butler <eric@kimbutler.xyz>
2022-04-04 14:12:01 +0200

    Add index.html Minification

--- packages/solid-a11y-docs/vite-plugins/index-html.ts
+++ packages/solid-a11y-docs/vite-plugins/index-html.ts
@@ -1,3 +1,4 @@
+import { minify } from "html-minifier-terser";
 import type { Plugin } from "vite";
 
 const ANALYTICS_SCRIPT = `<script src="https://thorn-side-absolutely.solid-a11y.dev/script.js" data-site="XSKZAHPM" defer></script>`;
@@ -11,7 +12,13 @@ export default function solidA11yIndexHTMLPlugin(): Plugin {
     },
     transformIndexHtml(html) {
       if (isProduction) {
-        return html.replace("</body>", `${ANALYTICS_SCRIPT}</body>`);
+        return minify(html.replace("</body>", `${ANALYTICS_SCRIPT}</body>`), {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+        });
       }
       return html;
     },

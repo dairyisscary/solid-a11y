bf5e44f30a3fbada9dcf6ff79403c194aa203dc2
Eric Kim-Butler <eric@kimbutler.xyz>
2022-04-06 18:56:08 +0200

    Fix Code HTML

--- packages/solid-a11y-docs/src/code.tsx
+++ packages/solid-a11y-docs/src/code.tsx
@@ -9,9 +9,9 @@ export function TerminalLines(props: TerminalLinesProps) {
     <code class="m-2 inline-block rounded-xl bg-slate-800 p-3 text-slate-200 shadow-lg ring-1 ring-inset ring-white/10">
       <For each={props.lines}>
         {(line) => (
-          <div>
+          <span class="block">
             <span class="text-pink-400">$</span> {line}
-          </div>
+          </span>
         )}
       </For>
     </code>
